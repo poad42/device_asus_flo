8fd169121b50315fca0a44b4785e516972f061bb
Adhitya Mohan <me@adhityamohan.in>
2017-02-18 19:05:42 +0000

    Revert "Netflix Hack"
    This reverts commit d34cccc60b6fe50c90bc071104984341cd2cdfc6.

--- device-common.mk
+++ device-common.mk
@@ -101,14 +101,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     lights.msm8960
 
-#NetflixHack
- PRODUCT_COPY_FILES += \
-  device/asus/flo/configs/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
-
 # Media
 PRODUCT_COPY_FILES += \
     device/asus/flo/configs/media_profiles.xml:system/etc/media_profiles.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     device/asus/flo/configs/media_codecs.xml:system/etc/media_codecs.xml \
     device/asus/flo/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml
 
